7caed61eca984dfcd859b30166597dd4e7393ff6
CheKecha19 <61182912+CheKecha19@users.noreply.github.com>
2020-03-04 09:28:21 +0300

    Add files via upload

---
+++ демонстрация.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace Электронные_деньги
+{
+    /// <summary>
+    /// Логика взаимодействия для демонстрация.xaml
+    /// </summary>
+    public partial class демонстрация : Window
+    {
+        public демонстрация()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+
+                        
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+    }
+}
